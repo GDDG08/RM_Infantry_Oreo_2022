a7e41fef39fbc44285515a15e61cef7791b99b04
shiyangxi <13610141922@139.com>
2022-05-04 09:41:12 +0800

    Caution: code=infantry7

--- Core/Src/Peripheral/key_periph.c
+++ Core/Src/Peripheral/key_periph.c
@@ -37,7 +37,8 @@ uint8_t Key_GetEquipCode() {
     // if (GPIO_ReadPin(CODE3) == GPIO_PIN_RESET) {
     //     code |= 0x04;
     // }
-    code = 0x07;
+    code = 0x07;    //for infantry 3
+    // code = 0x06; //for infantry 4?5
 #else
     BusComm_BusCommDataTypeDef* buscomm = BusComm_GetBusDataPtr();
     code = buscomm->infantry_code;
